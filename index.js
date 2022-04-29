de36877aba13840c69cbcd0ece53d093416aeb22
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-04-29 19:42:21 +0600

    mongodb connection done

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
             require('dotenv').config();
 
 // MiddleWare 
 app.use(cors());
 app.use(express.json());           
+
+// EcoHub
+// dKoHkpONM8nJUd8s
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7lgnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+app.listen(port, () => {
+    console.log('Running my port is ',port);
+})
