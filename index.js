0a634d3d979d9e11fde70fdc2c720ce2a61e7ab5
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-04-29 19:23:28 +0600

    initial structure done like express cors mongodb

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+            require('dotenv').config();
+
+// MiddleWare 
+app.use(cors());
+app.use(express.json());           
