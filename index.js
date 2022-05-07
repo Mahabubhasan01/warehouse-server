c857e96ca452a2b6fbb1710ddb829150097daa8b
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-07 11:24:59 +0600

    some issu solved

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000;
+const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
             require('dotenv').config();
 
 // MiddleWare 
